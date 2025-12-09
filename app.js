9b8c9d3adacca6f13d6b29c93bfd92e77ed20a54
carlyhuberr <carlyhuberr@gmail.com>
2025-12-09 16:44:58 +0300

    testing login

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const msg = document.getElementById("mess")
+// const msg = document.getElementById("mess")
+const logbut = document.getElementById("loginb")
+const enterPK = document.getElementById("enteredPK")
+const error = document.getElementById("error")
 
-document.getElementById("testButton").onclick = () => {
-    if (msg.textContent === "" ) {
-        msg.textContent = "button!";
+logbut.onclick = () => {
+    const pass = enterPK.value;
+
+    if (pass === "Huber") {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("homepage").style.display = "block";
     } else {
-        msg.textContent = "";
+        error.textContent = "Wrong passcode!";
+        error.style.color = "red";
     }
 };
+// document.getElementById("testButton").onclick = () => {
+//     if (msg.textContent === "" ) {
+//         msg.textContent = "button!";
+//     } else {
+//         msg.textContent = "";
+//     }
+// };
